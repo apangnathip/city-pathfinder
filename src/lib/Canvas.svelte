3896575ae2fd48dfc03b28aa2e7f4038c3d885d0
apangnathip <alanpangnathip@gmail.com>
2024-02-03 21:31:06 -0500

    change 2D canvas to webgl-based rendering

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -9,7 +9,7 @@
 
   let canvas: HTMLCanvasElement;
   let container: HTMLElement;
-  let ctx: CanvasRenderingContext2D | null;
+  let gl: WebGL2RenderingContext;
   let system: System;
 
   const mouse = {
@@ -22,25 +22,132 @@
     scrollScale: 1,
   };
 
+  function createShader(
+    gl: WebGL2RenderingContext,
+    type: number,
+    source: string,
+  ) {
+    const shader = gl.createShader(type);
+    if (!shader) return;
+
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+    if (success) {
+      return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+  }
+
+  function createProgram(
+    gl: WebGL2RenderingContext,
+    vertexShader: WebGLShader,
+    fragmentShader: WebGLShader,
+  ) {
+    const program = gl.createProgram();
+    if (!program) return;
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+
+    if (success) {
+      return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+  }
+
+  const vertexShaderSource = `#version 300 es
+    in vec2 a_position;
+    
+    uniform vec2 u_resolution;
+    
+    void main() {
+      vec2 clipSpace = (a_position / u_resolution) * 2.0 - 1.0;
+      gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+    }
+  `;
+
+  const fragmentShaderSource = `#version 300 es
+    precision highp float;
+
+    uniform vec4 u_color;
+
+    out vec4 outColor;
+
+    void main() {
+      outColor = u_color;
+    }
+  `;
+
   onMount(async () => {
     resize();
-    ctx = canvas.getContext("2d");
-    if (!ctx || !canvas) return;
+    gl = canvas.getContext("webgl2")!;
+    if (!gl || !canvas) return;
 
     system = new System(bbox, canvas.height, initialMapScaling);
     const graph = new Graph(osm.elements, system);
 
-    const animate = () => {
-      if (!ctx || !canvas) return;
-      requestAnimationFrame(animate);
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const vs = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    const fs = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+    if (!vs || !fs) return;
 
-      graph.drawEdges(ctx);
-      graph.drawNodes(ctx, true);
+    const program = createProgram(gl, vs, fs);
+    if (!program) return;
+
+    gl.useProgram(program);
+
+    //
+    // const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+    // gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+    //
+    // const colorLocation = gl.getUniformLocation(program, "u_color");
+    // gl.uniform4f(colorLocation, 0, 0, 0, 1);
+    //
+    // gl.drawArrays(gl.LINES, 0, positions.length / 2);
+
+    const uniform = {
+      resolution: gl.getUniformLocation(program, "u_resolution"),
+      color: gl.getUniformLocation(program, "u_color"),
+    };
+
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    const positionAttribLoc = gl.getAttribLocation(program, "a_position");
+    gl.enableVertexAttribArray(positionAttribLoc);
+    gl.vertexAttribPointer(positionAttribLoc, 2, gl.FLOAT, false, 0, 0);
+
+    const positions = graph.getEdgePositions();
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array(positions),
+      gl.DYNAMIC_DRAW,
+    );
+
+    const animate = () => {
+      if (!gl || !canvas) return;
+      requestAnimationFrame(animate);
+
+      // graph.drawEdges(gl);
+      // graph.drawNodes(gl, true);
       handleMouse(system);
+
+      gl.uniform2f(uniform.resolution, canvas.width, canvas.height);
+      gl.uniform4f(uniform.color, 0, 0, 0, 1);
+      gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+      gl.drawArrays(gl.LINES, 0, positions.length / 2);
     };
 
     animate();
+    // graph.drawEdgesGL(gl);
   });
 
   const resize = () => {
